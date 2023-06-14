c543f84a54dfb7d07318a6c3c63fc8479e587096
David Mackey-Ward <biggie9925@hotmail.com>
2023-06-14 20:22:03 +1200

    add to cart

---
+++ frontend/src/screens/CartScreen.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react'
+import { Link, useParams } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
+import { Message } from '../components/Message'
+import { addToCart } from '../actions/cartActions'
+
+function CartScreen(match, location, history) {
+
+  const {id} = useParams()
+  const productId = id
+  const qty = location.search ? Number(location.search.split('=')[1]) : 1
+
+  const dispatch = useDispatch()
+
+  const cart = useSelector(state => state.cart)
+  const {cartItems} = cart
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty))
+    }
+  }, [dispatch, productId, qty])
+
+  return (
+    <div>
+      Cart
+    </div>
+  )
+}
+
+export default CartScreen
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,14 +3,21 @@ import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
+import { cartReducer } from './reducers/cartReducers'
 
 
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
+    cart: cartReducer
 })
 
-export const initialState = {}
+const cartItemsFromStorage = localStorage.getItem('cartItems') ? 
+    JSON.parse(localStorage.getItem('cartItems')) : []
+
+export const initialState = {
+    cart:{ cartItems: cartItemsFromStorage }
+}
 
 const middleware = [thunk]
 
